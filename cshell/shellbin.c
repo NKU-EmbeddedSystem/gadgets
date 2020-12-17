9f2ebeee22bb267a388adb36aca9a9ae3ca121ba
jiangzhang <jiangzhangsg@qq.com>
2020-12-17 13:26:28 +0800

    js gadgets

---
+++ cshell/shellbin.c
@@ -0,0 +1,12 @@
+int main() {
+  asm("\
+needle0: jmp there\n\
+here:    pop %rdi\n\
+         mov $0x3b, %rax\n\
+         mov $0, %rsi\n\
+         mov $0, %rdx\n\
+         syscall\n\
+there:   call here\n\
+.string \"/bin/bash\"\n\
+  ");
+}
