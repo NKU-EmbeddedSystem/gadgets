8262ad36ee8856d304de8d35a0746934a59fff83
JerryMouseZ <1711333@mail.nankai.edu.cn>
2021-01-07 22:34:14 +0800

    traverse all group

--- gen_gadget/gen_gadget.py
+++ gen_gadget/gen_gadget.py
@@ -3,6 +3,44 @@ import re
 import sys
 import pathlib
 
+
+gadgets = ['5ec3', '5ac3']
+dd = [0, 0]
+exec_path = ''
+
+def excute_js(js:str)->bool:
+    f = open('test.js', 'w')
+    f.write(js)
+    f.close()
+    os.system(exec_path + ' --print-opt-code test.js > test.txt')
+    f = open('test.txt', 'r')
+    lines = f.readlines()
+    f.close()
+    for line in lines:
+        for jsc in gadgets:
+            if line.find(jsc) != -1:
+                print(line)
+                os.system('cp test.js ' + jsc + str(dd[gadgets.index(jsc)]) + '.js')
+                dd[gadgets.index(jsc)] += 1
+                return True
+    return False
+
+def search_word(word:str, lines:list):
+    result = ''
+    for line in lines:
+        if line.find(word) != -1:
+            result += line
+    return result
+
+def analyse(count:int):
+    f = open('test.txt', 'r')
+    lines = f.readlines()
+    f.close()
+    result = ''
+    for i in range(111, 114 + count):
+        result += search_word(',0x'+str(i), lines)
+    return result
+
 def generate_js(count:int):
     ops = ['&', '+', '*', '^']
     header = '''
@@ -14,17 +52,25 @@ function syscall_jsc(var1, var2, var3, var4){
 '''
 
     base = 114
-    middle = ''
+    middle1 = ''
     for i in range(count):
         var = 't' + str(i)
-        middle += '\tvar ' + var + ' = var1 ' + ops[i % len(ops)] + ' 0x' + str(base) + ';\n'
+        middle1 += '\tvar ' + var + ' = var1 ' + ops[i % len(ops)] + ' 0x' + str(base) + ';\n'
         base += 1
-    middle += '\tvar s = t' + str(count - 2) + ' + t' + str(count - 1) + ' * 2 + 0xc3;\n    return a0 | a1 + a2 | '
+    
+    jscs = []
+    for i in range(count):
+        for j in range(count):
+            if i == j:
+                continue
+            jsc = '\tvar s = t' + str(i) + ' + t' + str(j) + ' * 2 + 0xc3;\n    return a0 | a1 + a2 | '
+            jscs.append(jsc)
 
+    middle2 = ''
     op2s = ['+', '|']
     for i in range(count - 1):
-        middle += 't' + str(i) + ' ' + op2s[i % len(op2s)] + ' '
-    middle += 't' + str(count-1) + '\n}'
+        middle2 += 't' + str(i) + ' ' + '+ '
+    middle2 += 't' + str(count-1) + ' | s;\n}'
 
     tail = '''
 for(var i = 0; i < 0x10000; i++)
@@ -32,29 +78,10 @@ for(var i = 0; i < 0x10000; i++)
     syscall_jsc(0xc35, 0xc22, 0xc32, 0xc55);
 }
 '''
-    return header + middle + tail
+    for jsc in jscs:
+        excute_js(header + middle1 + jsc + middle2 + tail)
+    # return header + middle1 + jsc + middle2 + tail
 
-def excute_js(js:str, gadget:str, path:str):
-    f = open('test.js', 'w')
-    f.write(js)
-    f.close()
-    os.system(path + ' --print-opt-code test.js > test.txt')
-
-def search_word(word:str, lines:list):
-    result = ''
-    for line in lines:
-        if line.find(word) != -1:
-            result += line
-    return result
-
-def analyse(gadget:str, count:int):
-    f = open('test.txt', 'r')
-    lines = f.readlines()
-    f.close()
-    result = ''
-    for i in range(111, 114 + count):
-        result += search_word(',0x'+str(i), lines)
-    return result
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -67,15 +94,10 @@ if __name__ == "__main__":
         print('path of v8 error')
         exit()
 
-    gadgets = ['58c3', '5fc3', '5ec3', '5ac3', '0f05'] 
 
     for i in range(32):
-        f = open(str(i) + '.txt', 'w')
-        test_js = generate_js(i)
-        f2 = open(str(i) + '.js', 'w')
-        f2.write(test_js)
-        f2.close()
-        excute_js(test_js, gadgets[0], exec_path)
-        f.write(analyse(gadgets[0], i))
-        f.close()
+        generate_js(i)
+        # excute_js(test_js)
+        # f.write(analyse(i))
+        # f.close()
     print('done')
