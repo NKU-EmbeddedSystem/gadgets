5b8ab48ea9c792127aaf6afe0003d8be700ecf98
jiangzhang <jiangzhangsg@qq.com>
2021-01-07 14:03:20 +0800

    auto generate gadgets

--- no_constant/test.js
+++ no_constant/test.js
@@ -2,25 +2,20 @@ var array = new Uint8Array();
 // %DebugPrint(array);
 
 function syscall_jsc(var1, var2, var3, var4){
-	var rdi = var1 | 0xc3;
-	var rbx = var1 ^ 0xc3;
-	var rcx = var1 & 0xc3;
-	var r8 = var1 + 0xc3;
-	var r9 = var1 * 0xc3;
-	var a = var2 - 0xc2;
-	var b = var2 % 0xc2;
-	var c = var2 * 0xc2;
-	var d = var2 - 0xc2;
-	var e = var2 ^ 0xc2;
-
+	var rdi = var1 | 0x123;
+	var rcx = var1 & 0x234;
+	var b = var2 % 0x345;
+	var c = var2 * 0x456;
+	var e = var2 ^ 0x567;
+	
 	//release rdi and rcx
-	e += rdi * 2;
-	e += rcx;
+	e += rdi;
+	e += rcx * 2;
 
 	//force to use rdi and rcx
-	var extra = e + 0x05;
+	var extra = e + 0xc3;
 
-	var result = r8 + r9 + rbx + a + b + c + d | extra;
+	var result = b + c | extra;
 	return result;
 }
 
