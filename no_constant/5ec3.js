c254ac6decb3b64a2b9a709de2973f60b42b37eb
JerryMouseZ <1711333@mail.nankai.edu.cn>
2021-02-17 20:50:18 +0800

    sib gadgets

--- no_constant/5ec3.js
+++ no_constant/5ec3.js
@@ -1,6 +1,5 @@
 
-var array = new Uint8Array();
-function syscall_jsc(var1, var2, var3, var4){    
+function f5ec3(var1, var2, var3, var4){    
     var a0 = var1 ^ 0x111;
     var a1 = var1 ^ 0x112;
     var a2 = var1 ^ 0x113;
@@ -25,5 +24,5 @@ function syscall_jsc(var1, var2, var3, var4){
 }
 for(var i = 0; i < 0x10000; i++)
 {
-    syscall_jsc(0xc35, 0xc22, 0xc32, 0xc55);
+    f5ec3(0xc35, 0xc22, 0xc32, 0xc55);
 }
