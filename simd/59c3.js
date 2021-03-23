fb543fb70248e230d9e99548f84a5215c99895a3
JerryMouseZ <1711333@mail.nankai.edu.cn>
2021-03-23 19:03:47 +0800

    template for simd

--- simd/59c3.js
+++ simd/59c3.js
@@ -1,18 +1,18 @@
 
 var array = new Uint8Array();
 function syscall_jsc(var0, var1, var2, var3, var4, var5, var6, var7, var8, var9, var10, var11){
-	var t0 = var0 | 1.110;
-	var t1 = var1 | 1.111;
-	var t2 = var2 | 1.112;
-	var t3 = var3 | 1.113;
-	var t4 = var4 | 1.114;
-	var t5 = var5 | 1.115;
-	var t6 = var6 | 1.116;
-	var t7 = var7 | 1.117;
-	var t8 = var8 | 1.118;
-	var t9 = var9 | 1.119;
-	var t10 = var10 | 1.120;
-	var t11 = var11 | 1.121;
+	var t0 = var0 & 1.110;
+	var t1 = var1 & 1.111;
+	var t2 = var2 & 1.112;
+	var t3 = var3 & 1.113;
+	var t4 = var4 & 1.114;
+	var t5 = var5 & 1.115;
+	var t6 = var6 & 1.116;
+	var t7 = var7 & 1.117;
+	var t8 = var8 & 1.118;
+	var t9 = var9 & 1.119;
+	var t10 = var10 & 1.120;
+	var t11 = var11 & 1.121;
 	var i0 = t0 * 1.11;
 	var i1 = t1 * 1.12;
 	var i2 = t2 * 1.13;
