3e287161c2839435fc49c46512f354f2345ceed0
jz <jiangzhangsg@qq.com>
2021-05-17 14:13:37 +0800

    scripts calculating coding space

--- modgadgets/calculate.py
+++ modgadgets/calculate.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 # coding=utf-8
+def myhex(num):
+    res = hex(num).upper()[2:]
+    if len(res) == 1:
+        res = '0' + res
+    return res
 def calculate(mod1, mod2, reg, rm):
     i = 0
     left = 0
@@ -17,10 +22,10 @@ def printb(vectors):
     for i in range(0x100):
         for vector in vectors:
             if i >= vector[0] and i <= vector[1]:
-                print(hex(i)[2:], end='')
+                print(r'\textcolor{red}'+ '{' + myhex(i) + '}', end='')
                 break
         else:
-            print(r'\textcolor{red}'+ '{' + hex(i)[2:] + '}', end='')
+            print(myhex(i), end='')
 
         if (i+1) % 0x10 == 0:
             print(r'\\')
